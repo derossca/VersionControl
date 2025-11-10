557682b7049d402e7bbf96c22c062dbe429b0e21
Corey DeRossett <155482837+derossca@users.noreply.github.com>
2025-11-09 21:13:08 -0500

    Initial push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VersionControl.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
